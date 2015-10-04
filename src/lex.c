a9f8100ca22e254a138e7228dd7d98ae972eb691
Frantisek Sumsal <frantisek@sumsal.cz>
2015-10-04 21:23:13 +0200

    Add basic parser

--- src/lex.c
+++ src/lex.c
@@ -1,6 +1,8 @@
 /* Draft of lexical analysis module */
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
 
 // Main function here is only for testing purposes
 int main(int argc, char *argv[])
@@ -17,18 +19,45 @@ int main(int argc, char *argv[])
         exit(1);
     }
     
-    char c = '\0';
+    int c = '\0';
+    int la = '\0';
     int linecount = 0;
 
     while((c = fgetc(in)) != EOF) {
+        // Skip spaces and tabs
         if(c == ' ' || c == '\t')
             continue;
+        // Skip newlines and increase line counter
         if(c == '\n') {
             linecount++;
             continue;
         }
 
-        
+        if(c == '/') {
+            if((la = fgetc(in)) != EOF) {
+                // Skip one line comments
+                if(la == '/') {
+                    while((c = fgetc(in)) != '\n' && c != EOF);
+                    continue;
+                // Skip multiline comments
+                } else if(la == '*') {
+                    bool starFound = false; 
+                    while((c = fgetc(in)) != EOF) {
+                        if(c == '*')
+                            starFound = true;
+                        else if(starFound && c == '/')
+                            break;
+                        else
+                            starFound = false;
+                    }
+                    continue;
+                } else {
+                    ungetc(la, in);
+                }
+            }
+        }
+      
+        fputc(c, stdout);
     }    
 
     printf("%d lines\n", linecount);
