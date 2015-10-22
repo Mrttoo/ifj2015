378e480afc2cd3185a72e3ed5968aa1770d87e66
Frantisek Sumsal <frantisek@sumsal.cz>
2015-10-22 12:39:48 +0200

    Initial BST draft

---
+++ src/bst.c
@@ -0,0 +1,84 @@
+/**
+  *  Draft of Binary Search Tree
+  *  which will be used for implementation
+  *  of symbol table
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "bst.h"
+#include "error.h"
+
+bst_node_t *bst_new_node(int data)
+{
+    bst_node_t *n = malloc(sizeof *n);
+
+    if(n == NULL) {
+        fprintf(stderr, "Unable to allocate memory for BST node\n");
+        exit(IFJ_INTERNAL_ERR);
+    }
+
+    n->left = NULL;
+    n->right = NULL;
+    n->data = data;
+
+    return n;
+}
+
+void bst_destroy_tree(bst_node_t *node)
+{
+    if(node == NULL) 
+        return;
+    if(node->left != NULL)
+        bst_destroy_tree(node->left);
+    if(node->right != NULL)
+        bst_destroy_tree(node->right);
+
+    free(node);
+}
+
+bst_node_t *bst_insert_node(bst_node_t *node, int data)
+{
+    if(node == NULL) {
+        return bst_new_node(data);
+    } else {
+        if(data <= node->data)
+            node->left = bst_insert_node(node->left, data);
+        else
+            node->right = bst_insert_node(node->right, data);
+    }
+
+    return node;
+}
+
+#ifdef IFJ_BST_DEBUG
+
+void bst_print_tree(bst_node_t *node)
+{
+    if(node == NULL)
+        return;
+
+    bst_print_tree(node->left);
+    printf("%d\n", node->data);
+    bst_print_tree(node->right);
+}
+
+int main(int argc, char *argv[])
+{
+    bst_node_t *root = NULL;
+
+    root = bst_new_node(10);
+    root = bst_insert_node(root, 4);
+    root = bst_insert_node(root, 8);
+    root = bst_insert_node(root, 1);
+    root = bst_insert_node(root, 5);
+    root = bst_insert_node(root, 2);
+
+    bst_print_tree(root);
+
+    bst_destroy_tree(root);
+
+    return 0;
+}
+#endif
---
+++ src/bst.h
@@ -0,0 +1,25 @@
+#ifndef __BST_H_INCLUDED
+#define __BST_H_INCLUDED
+
+/**
+  *  Draft of Binary Search Tree
+  *  which will be used for implementation
+  *  of symbol table
+*/
+
+//typedef struct bst_node_key {
+//    // Test
+//    char data[32];
+//} bst_node_key_t;
+
+typedef struct bst_node {
+    struct bst_node *left;
+    struct bst_node *right;
+    int data;    
+} bst_node_t;
+
+bst_node_t *bst_new_node(int data);
+void bst_destroy_tree(bst_node_t *node);
+bst_node_t *bst_insert_node(bst_node_t *node, int data);
+
+#endif
