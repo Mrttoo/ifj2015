697b796c2e7b315988b92dd5139c83ab4834f8ce
Frantisek Sumsal <frantisek@sumsal.cz>
2015-10-22 14:52:12 +0200

    Add simple unit tests

--- src/bst.c
+++ src/bst.c
@@ -80,19 +80,39 @@ void bst_print_tree(bst_node_t *node)
     bst_print_tree(node->right);
 }
 
+#define TVC 10
 int main(int argc, char *argv[])
 {
+    int tv[TVC] = { 6, 1, 3, 8, 15, 4, 65, 23, 7, 0 };
     bst_node_t *root = NULL;
 
+    // Test alloc
     root = bst_new_node(10);
-    root = bst_insert_node(root, 4);
-    root = bst_insert_node(root, 8);
-    root = bst_insert_node(root, 1);
-    root = bst_insert_node(root, 5);
-    root = bst_insert_node(root, 2);
 
+    // Test insert
+    for(unsigned int i = 0; i < TVC; i++)
+        root = bst_insert_node(root, tv[i]);
+
+    // Print tree
     bst_print_tree(root);
 
+    // Test lookup
+    for(unsigned int i = 0; i < TVC; i++) {
+        bst_node_t *search = bst_lookup_node(root, tv[i]);
+        if(search == NULL) {
+            fprintf(stderr, "[FAIL] Couldn't find node with value %d\n", tv[i]);
+        } else {
+            if(tv[i] == search->data) {
+                fprintf(stderr, "[PASS] Found node with correct value (%d == %d)\n",
+                        tv[i], search->data);
+            } else {
+                fprintf(stderr, "[FAIL] Found node with incorrect value (%d != %d)\n",
+                        tv[i], search->data);
+            }
+        }
+    }
+
+    // Test free (check with valgrind)
     bst_destroy_tree(root);
 
     return 0;
