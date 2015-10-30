982aaefdf2d2edfee32f312527f78ba8c276ebf8
Frantisek Sumsal <frantisek@sumsal.cz>
2015-10-30 12:26:11 +0100

    Syntax analyser - initial commit

---
+++ src/syntax.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "syntax.h"
+#include "error.h"
+
+#ifdef IFJ_SYNTAX_DEBUG
+
+int main()
+{
+
+    return 0;
+}
+
+#endif
---
+++ src/syntax.h
@@ -0,0 +1,6 @@
+#ifndef __SYNTAX_H_INCLUDED
+#define __SYNTAX_H_INCLUDED
+
+#include <stdlib.h>
+
+#endif
