6d953cdae3911cb4efad891ecd12253ac1c3246c
Frantisek Sumsal <frantisek@sumsal.cz>
2015-11-21 00:57:38 +0100

    Test file for syntax analysis

---
+++ src/syntaxtest.c
@@ -0,0 +1,46 @@
+int main();
+
+string test(int p)
+{
+    int test;
+    string sdfasd223_234222;
+    double _num;
+    auto testiceq = test;
+
+    test = _num;
+
+    if(test) {
+        _num = test;
+    } else {
+        _num = testiceq;
+    }
+
+    test = some_function();
+    test = some_function2(test);
+    test = some_function3(test, test, test);
+    test = some_function4("test", 2.01, 23, _num);
+
+    cin >> test;
+    cin >> test >>_num >> testiceq;
+
+    cout << test;
+    cout << test << test << test;
+    cout << 1 << 1.2 << "test" << _num;
+
+    for(int i = _num; _num; i = _num) {
+        if(_num) {
+            _num = test;
+            return;
+        } else {
+            testiceq = test;
+        }
+    }
+
+    return test;
+}
+
+double test2(int a, int b, string c)
+{ 
+
+}
+
