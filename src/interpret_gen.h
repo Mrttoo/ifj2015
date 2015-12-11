418a583154e2c826cac40262b890a4f600a5b6f4
Frantisek Sumsal <frantisek@sumsal.cz>
2015-12-11 20:41:05 +0100

    Fix memory leaks & segfaults

--- src/interpret_gen.h
+++ src/interpret_gen.h
@@ -60,35 +60,4 @@ void instr_jump_to(instr_list_t *list, instr_list_item_t *instr);
 void instr_jump_next(instr_list_t *list);
 instr_t *instr_active_get_data(instr_list_t *list);
 
-typedef struct instr_stack {
-    int size;
-    int first_idx;
-    int free_idx;
-    stable_variable_t *items;
-} instr_stack_t;
-
-void instr_stack_init(instr_stack_t *stack, unsigned int size);
-void instr_stack_push(instr_stack_t *stack, stable_variable_t *instr);
-stable_variable_t *instr_stack_pop_first(instr_stack_t *stack);
-stable_variable_t *instr_stack_pop(instr_stack_t *stack);
-
-typedef struct {
-    void *ret_addr;
-    int ret_ip;
-    stable_variable_t ret_val;
-    instr_stack_t vars;
-} frame_t;
-
-typedef struct frame_stack {
-    int size;
-    int free_idx;
-    frame_t **items;
-} frame_stack_t;
-
-void frame_stack_init(frame_stack_t *stack);
-frame_t *frame_stack_new(frame_stack_t *stack, unsigned int var_count);
-void frame_stack_push(frame_stack_t *stack, frame_t *frame);
-frame_t *frame_stack_pop(frame_stack_t *stack);
-frame_t *frame_stack_get_top(frame_stack_t *stack);
-
 #endif
