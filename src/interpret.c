65841d01a2cbff50d95847e0cf84b060f4205268
Frantisek Sumsal <frantisek@sumsal.cz>
2015-12-11 20:41:05 +0100

    Fix memory leaks & segfaults

--- src/interpret.c
+++ src/interpret.c
@@ -4,6 +4,7 @@
 
 #include "interpret_gen.h"
 #include "interpret_funcs.h"
+#include "interpret.h"
 #include "stable.h"
 #include "ef.h"
 
@@ -19,9 +20,18 @@ int interpret_process(instr_list_t *instr_list)
 
     for(;;) {
         instr = instr_active_get_data(instr_list);
+        printf("\nCurr instr: %d\n", instr->type);
 
         switch(instr->type) {
         case INSTR_HALT:
+            // Clean frame stack
+            while((tmp_frame = frame_stack_pop(&fstack)) != NULL) {
+                frame_destroy(tmp_frame);
+                tmp_frame = NULL;
+            }
+
+            free(fstack.items);
+
             return 0;
         break;
         case INSTR_LENGTH:
@@ -51,7 +61,7 @@ int interpret_process(instr_list_t *instr_list)
             puts("CREATING NEW STACK FRAME");
             tmp_frame = frame_stack_new(&fstack, instr->addr3);
             if(curr_frame != NULL) {
-                tmp_frame->ret_addr = (void*)instr_list->active->next;
+                tmp_frame->ret_addr = (void*)instr_list->active;
             }
 
             curr_frame = tmp_frame;
@@ -61,13 +71,15 @@ int interpret_process(instr_list_t *instr_list)
             frame_stack_pop(&fstack);
             tmp_frame = frame_stack_get_top(&fstack);
             // TODO: Assign return value, delete pop'd frame
-            instr_list->active = (instr_list_item_t*)&(instr->addr1);
+            instr_list->active = (instr_list_item_t*)curr_frame->ret_addr;
+            frame_destroy(curr_frame);
+            curr_frame = tmp_frame;
         break;
         case INSTR_PUSHF:
 
         break;
         case INSTR_MOVI:
-            curr_frame->vars.items[instr->addr1].val.i = curr_frame->vars.items[instr->addr2].val.i;
+            curr_frame->vars.items[instr->addr1].val.i = instr->addr2;//curr_frame->vars.items[instr->addr2].val.i;
             curr_frame->vars.items[instr->addr1].dtype = STABLE_INT;
         break;
         case INSTR_MOVD:
@@ -94,9 +106,9 @@ int interpret_process(instr_list_t *instr_list)
             instr_list->active = (instr_list_item_t*)&(instr->addr1);
         break;
         case INSTR_JMPC:
-            // If addr1 value is true, jump to label
-            //if(((stable_variable_t*)instr->addr1)->val.i)
-             //   instr_list->active = (instr_list_item_t*)instr->addr3;
+            //If addr1 value is true, jump to label
+            if(curr_frame->vars.items[instr->addr1].val.i)
+                instr_list->active = (instr_list_item_t*)instr->addr2;
         break;
         case INSTR_LAB:
             // Label instruction, do nothing
@@ -110,16 +122,23 @@ int interpret_process(instr_list_t *instr_list)
 int main(int argc, char *argv[])
 {
     instr_list_t list;
-
     instr_list_init(&list);
-    instr_list_item_t *ptr = NULL;
+    instr_list_item_t *ptr = NULL, *ptr2 = NULL, *ptr3 = NULL, *entry = NULL;
 
     ptr = instr_insert_instr(&list, INSTR_LAB, 0, 0, 0);
-    instr_insert_before_instr(&list, ptr, INSTR_CALL, (intptr_t)ptr, 0, 2);
-    instr_insert_instr(&list, INSTR_CIN, 1, STABLE_STRING, 0);
-    instr_insert_instr(&list, INSTR_COUT, 1, 0, 0);
-    instr_insert_instr(&list, INSTR_CIN, 0, STABLE_INT, 0);
+    instr_insert_instr(&list, INSTR_MOVI, 0, 99, 0);
     instr_insert_instr(&list, INSTR_COUT, 0, 0, 0);
+    instr_insert_instr(&list, INSTR_RET, 0, 0, 0);
+
+    ptr2 = instr_insert_instr(&list, INSTR_LAB, 0, 0, 0);
+
+    entry = instr_insert_before_instr(&list, ptr2, INSTR_CALL, (intptr_t)ptr2, 0, 2);
+    //instr_insert_instr(&list, INSTR_CIN, 1, STABLE_STRING, 0);
+    //instr_insert_instr(&list, INSTR_COUT, 1, 0, 0);
+    ptr3 = instr_insert_after_instr(&list, ptr2, INSTR_CIN, 0, STABLE_INT, 0);
+    ptr3 = instr_insert_after_instr(&list, ptr3, INSTR_COUT, 0, 0, 0);
+    ptr3 = instr_insert_after_instr(&list, ptr3, INSTR_JMPC, 0, (intptr_t)ptr2, 0);
+    ptr3 = instr_insert_after_instr(&list, ptr3, INSTR_CALL, (intptr_t)ptr, 0, 1);
     instr_insert_instr(&list, INSTR_HALT, 0, 0, 0);
 
     instr_list_item_t *lptr = list.first;
@@ -127,41 +146,151 @@ int main(int argc, char *argv[])
         printf("Instruction: %d\n", lptr->data.type);
         lptr = lptr->next;
     }
-    
-   /* stable_variable_t var;
-    stable_variable_t var2 = { .dtype = STABLE_STRING, .val.s = "Zadejte cislo: " };
-    stable_variable_t var3 = { .dtype = STABLE_STRING, .val.s = "\n" };
-    stable_variable_t var4 = { .dtype = STABLE_INT, .val.i = -1 };
-    stable_variable_t var5 = { .dtype = STABLE_INT, .val.i = -1 };
 
-    stable_variable_t arg1 = { .dtype = STABLE_STRING, .val.s = "Test" };
-
-    instr_list_item_t *ptr = NULL;
-    instr_list_t list;
-    instr_stack_t s;
-
-    instr_stack_init(&s);
-    instr_stack_push(&s, &arg1);
-
-    var.dtype = STABLE_INT;
-    instr_list_init(&list);
-    ptr = instr_insert_instr(&list, INSTR_LAB, NULL, NULL, NULL);
-    instr_insert_instr(&list, INSTR_COUT, &var2, NULL, NULL);
-    instr_insert_instr(&list, INSTR_CIN, &var, NULL, NULL);
-    instr_insert_instr(&list, INSTR_COUT, &var, NULL, NULL);
-    instr_insert_instr(&list, INSTR_COUT, &var3, NULL, NULL);
-    instr_insert_instr(&list, INSTR_SUB, &var4, &var4, &var5);
-    instr_insert_instr(&list, INSTR_JMPC, &var4, NULL, ptr);
-    instr_insert_instr(&list, INSTR_LENGTH, &var4, NULL, &s);
-    instr_insert_instr(&list, INSTR_COUT, &var4, NULL, NULL);
-    instr_insert_instr(&list, INSTR_HALT, NULL, NULL, NULL);
-*/
-    list.active = list.first;
+    list.active = entry;
 
     int rc = interpret_process(&list);
 
     instr_list_destroy(&list);
 
     return rc;
-    return 0;
+}
+
+void instr_stack_init(instr_stack_t *stack, unsigned int size)
+{
+    stack->items = malloc(sizeof *(stack->items) * size);
+    if(stack->items == NULL) {
+        fprintf(stderr, "%s: Unable to allocate memory for stack\n", __func__);
+        exit(IFJ_INTERNAL_ERR);
+    }
+
+    stack->size = size;
+    stack->first_idx = 0;
+    stack->free_idx = 0;
+}
+
+void instr_stack_push(instr_stack_t *stack, stable_variable_t *var)
+{
+    if(stack == NULL || var == NULL)
+        return;
+
+    if((stack->free_idx + 1) == stack->size) {
+        stable_variable_t *n = realloc(stack->items, sizeof *(stack->items) * (stack->size + INTERPRET_STACK_CHUNK));
+        if(n == NULL) {
+            fprintf(stderr, "%s: Unable to expand stack\n", __func__);
+            exit(IFJ_INTERNAL_ERR);
+        }
+
+        stack->items = n;
+    }
+
+    stack->items[stack->free_idx++] = *var;
+}
+
+stable_variable_t *instr_stack_pop_first(instr_stack_t *stack)
+{
+    if(stack == NULL)
+        return NULL;
+
+    // Empty stack
+    if(stack->first_idx == stack->free_idx)
+        return NULL;
+
+    return &(stack->items[stack->first_idx--]);
+}
+
+stable_variable_t *instr_stack_pop(instr_stack_t *stack)
+{
+    if(stack == NULL)
+        return NULL;
+
+    // Empty stack
+    if(stack->first_idx == stack->free_idx)
+        return NULL;
+
+    return &(stack->items[--stack->free_idx]);
+}
+
+void frame_stack_init(frame_stack_t *stack)
+{
+    if(stack == NULL)
+        return;
+
+    stack->items = malloc(sizeof *(stack->items) * 10);
+    if(stack->items == NULL) {
+        fprintf(stderr, "%s: Unable to allocate memory for stack\n", __func__);
+        exit(IFJ_INTERNAL_ERR);
+    }
+
+    stack->size = 5;
+    stack->free_idx = 0;
+}
+
+void frame_stack_push(frame_stack_t *stack, frame_t *frame)
+{
+    if(stack == NULL || frame == NULL)
+        return;
+
+    if((stack->free_idx + 1) == stack->size) {
+        frame_t **n = realloc(stack->items, sizeof *(stack->items) * (stack->size + 10));
+        if(n == NULL) {
+            fprintf(stderr, "%s: Unable to reallocate stack array\n", __func__);
+            exit(IFJ_INTERNAL_ERR);
+        }
+
+        stack->items = n;
+        stack->size += 10;
+    }
+
+    stack->items[stack->free_idx++] = frame;
+}
+
+frame_t *frame_stack_pop(frame_stack_t *stack)
+{
+    if(stack == NULL)
+        return NULL;
+
+    // Empty stack
+    if(stack->free_idx == 0)
+        return NULL;
+
+    return stack->items[--stack->free_idx];
+}
+
+frame_t *frame_stack_get_top(frame_stack_t *stack)
+{
+    if(stack == NULL)
+        return NULL;
+
+    if(stack->free_idx == 0)
+        return NULL;
+
+    return stack->items[stack->free_idx - 1];
+}
+
+frame_t *frame_stack_new(frame_stack_t *stack, unsigned int var_count)
+{
+    if(stack == NULL)
+        return NULL;
+
+    frame_t *f = malloc(sizeof *f);
+    if(f == NULL) {
+        fprintf(stderr, "%s: Unable to allocate memory for a frame\n", __func__);
+        exit(IFJ_INTERNAL_ERR);
+    }
+
+    instr_stack_init(&f->vars, var_count);
+    frame_stack_push(stack, f);
+
+    return f;
+}
+
+void frame_destroy(frame_t *frame)
+{
+    if(frame == NULL)
+        return;
+
+    free(frame->vars.items);
+    frame->vars.items = NULL;
+    free(frame);
 }
--- src/interpret.h
+++ src/interpret.h
@@ -1,6 +1,39 @@
 #ifndef __INTERPRET_H_INCLUDED
 #define __INTERPRET_H_INCLUDED
 
+#include <stdint.h>
+#include "stable.h"
 
+typedef struct instr_stack {
+    int size;
+    int first_idx;
+    int free_idx;
+    stable_variable_t *items;
+} instr_stack_t;
+
+void instr_stack_init(instr_stack_t *stack, unsigned int size);
+void instr_stack_push(instr_stack_t *stack, stable_variable_t *instr);
+stable_variable_t *instr_stack_pop_first(instr_stack_t *stack);
+stable_variable_t *instr_stack_pop(instr_stack_t *stack);
+
+typedef struct {
+    void *ret_addr;
+    int ret_ip;
+    stable_variable_t ret_val;
+    instr_stack_t vars;
+} frame_t;
+
+typedef struct frame_stack {
+    int size;
+    int free_idx;
+    frame_t **items;
+} frame_stack_t;
+
+void frame_stack_init(frame_stack_t *stack);
+frame_t *frame_stack_new(frame_stack_t *stack, unsigned int var_count);
+void frame_stack_push(frame_stack_t *stack, frame_t *frame);
+frame_t *frame_stack_pop(frame_stack_t *stack);
+frame_t *frame_stack_get_top(frame_stack_t *stack);
+void frame_destroy(frame_t *frame);
 
 #endif
