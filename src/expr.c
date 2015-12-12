787c201de4e2362758566e41e1a45a59909f8fb3
Mrttoo <martinhonza123@gmail.com>
2015-12-12 20:45:26 +0100

    Alpha version of instruction

--- src/expr.c
+++ src/expr.c
@@ -7,14 +7,17 @@
 #include "expr.h"
 #include "Estack.h"
 #include "util.h"
-//#include "interpret_gen.h"
+#include "interpret_gen.h"
+
 
 #define ENUM_TO_STR(x) lex_token_strings[x - 256]
 
 #define syntax_error(...) throw_syntax_error(IFJ_SYNTAX_ERR, &lex_data, __VA_ARGS__);
 #define syntax_error_ec(ec, ...) throw_syntax_error(ec, &lex_data, __VA_ARGS__);
 
-stable_variable_t *constant_table;
+stable_const_t constant_table;
+instr_list_t list;
+int final_index;
 
 /*
    case precedence_table[stack_top][t->type]
@@ -95,7 +98,6 @@ sign_value get_sign(lex_token_t *t)
 	case LEX_IDENTIFIER:
 	case LEX_INTEGER:
 	case LEX_DOUBLE:
-	case LEX_STRING;
 		return symbol_id;
 	
 	case LEX_SEMICOLON:
@@ -114,55 +116,64 @@ sign_value get_sign(lex_token_t *t)
   
  */
 
-void type(Stack *stack, Stack *stack_types, int Rule)
+void type(Stack *stack, Stack *stack_types, Stack *stack_index, int instr_type_t)
 {
 	int second_op = stack_top(&(*stack_types));
 	stack_pop(&(*stack_types));
 	int first_op = stack_top(&(*stack_types));
 	stack_pop(&(*stack_types));
 
+	int first_addr;
+	int second_addr;
+
 
 	stack_pop(&(*stack));
 	stack_pop(&(*stack));
 	stack_pop(&(*stack));
 	stack_push(&(*stack), 'E');
+	
+	second_addr = stack_top(&(*stack_index));
+	stack_pop(&(*stack_index));
+	first_addr = stack_top(&(*stack_index));
+	stack_pop(&(*stack_index));
+	int offset = symbol_table.active->stack_idx++;
+	final_index = offset;
 
 
-
-	switch(Rule)
+	switch(instr_type_t)
 	{
-		case E_plus_E:
-		case E_minus_E:
-		case E_mul_E:
-		case E_div_E:
+		case INSTR_ADD:
+		case INSTR_SUB:
+		case INSTR_MUL:
+		case INSTR_DIV:
 			if((first_op == LEX_INTEGER) && (second_op == LEX_INTEGER))
-			{
-				//instr_insert_instr()
+			{				
+				instr_insert_instr(&list,instr_type_t, offset, first_addr, second_addr);
 				stack_push(&(*stack_types), LEX_INTEGER);
 				break;
 			}
 				
 			else
 			{
-				//instr_insert_instr()
+				instr_insert_instr(&list, instr_type_t, offset, first_addr, second_addr);
 				stack_push(&(*stack_types), LEX_DOUBLE);
 				break;
 			}
-		case E_less_E:
-		case E_greater_E:
-		case E_lessequal_E:
-		case E_greaterequal_E:
-		case E_equal_E:
-		case E_notequal_E:
+		case INSTR_LT:
+		case INSTR_GT:
+		case INSTR_LTE:
+		case INSTR_GTE:
+		case INSTR_EQ:
+		case INSTR_NEQ:
 				if((first_op == LEX_INTEGER) && (second_op == LEX_INTEGER)) 
 				{
-					//instr_insert_instr()
+					instr_insert_instr(&list, instr_type_t, offset, first_addr, second_addr);
 					stack_push(&(*stack_types), LEX_INTEGER);
 					break;
 				}
 				else
 				{
-					//instr_insert_instr()
+					instr_insert_instr(&list, instr_type_t, offset, first_addr, second_addr);
 					stack_push(&(*stack_types), LEX_DOUBLE);
 					break;
 				}
@@ -173,37 +184,33 @@ void type(Stack *stack, Stack *stack_types, int Rule)
 
 
 
-void constant_check(lex_token_t *token)
+void constant_check(lex_token_t *token, Stack *stack_index)
 {
-	static int i = 1;
-	if(i < 2)
-		constant_table = malloc(sizeof(stable_variable_t));
-	 if(constant_table == NULL)
-	 {
-		 fprintf(stderr,"Error while allocating in constant check");
-	 }
-	
+	int i;
+	double d;
+	int constant_offset;
 
 	switch(token->type)
 	{
 		case LEX_INTEGER:
-		 	constant_table[i].val.i = atoi(token->val);
-			constant_table[i].dtype = 0;
-			i++;
-			//instr_insert_instr(&list, INST_MOVI,0 ,0 ,i);
+		 	i = atoi(token->val);
+			constant_offset = stable_const_insert_int(&constant_table, i);
+			instr_insert_instr(&list, INSTR_MOVI, constant_offset, 0, 0);
+			stack_push(&(*stack_index), constant_offset);
 			break;
 
 		case LEX_DOUBLE:
-			constant_table[i].val.d  = atof(token->val);
-			i++;
-			constant_table[i].dtype = 1;
-			//inst_insert_instr(&list, INST_MOVD,0 ,0 ,i )
+			d = atof(token->val);
+			constant_offset = stable_const_insert_double(&constant_table, d);
+			instr_insert_instr(&list, INSTR_MOVD, constant_offset, 0, 0);
+			stack_push(&(*stack_index), constant_offset);
 			break;
+		
 		case LEX_STRING:
-			constant_table[i].val.s = token->val;
-			i++;
-			constant_table[i].dtype = 2;
-			//inst_insert_instr(&list, INST_MOVS,0, 0, i)
+			constant_offset = stable_const_insert_string(&constant_table, token->val);
+			instr_insert_instr(&list, INSTR_MOVS, constant_offset, 0, 0);
+			stack_push(&(*stack_index), constant_offset);
+			break;
 
 		default:
 			break;
@@ -220,20 +227,25 @@ void constant_check(lex_token_t *token)
    that it continues according to precedence rules (=, <, >).
  */
 
-lex_token_t *syntax_precedence(lex_token_t *token, lex_data_t *data, stable_data_t *symbol_data, stable_data_t
+int syntax_precedence(lex_token_t *token, lex_data_t *data, stable_data_t *symbol_data, stable_data_t
 		*ptr_data, syntax_data_t *syntax_data)
 {
 	int i=1;
-	int offset;
 
 	Stack stack;
 	Stack stack_types;
+	Stack stack_index;
 
 	Stack_Init(&stack);
 	Stack_Init(&stack_types);
 
 	stack_push(&stack, symbol_dollar);
 	stack_push(&stack_types, symbol_dollar);
+
+
+
+	stable_const_init(&constant_table);
+	//instr_list_init(&list);
 
 
 	do
@@ -243,6 +255,7 @@ lex_token_t *syntax_precedence(lex_token_t *token, lex_data_t *data, stable_data
 		//	fprintf(stderr,"Undefined variabile");
 
 		printf("[%s] Current token: (%d) %s\n", __func__, token->type, ENUM_TO_STR(token->type));
+
 		if(stack_top(&stack) == 'E')
 			i=2;
 		else
@@ -257,7 +270,6 @@ lex_token_t *syntax_precedence(lex_token_t *token, lex_data_t *data, stable_data
 				}
 				stack_push(&stack, get_sign(token));
 				lex_get_token(data, token);
-
 				break;
 
 			case '<':
@@ -266,32 +278,24 @@ lex_token_t *syntax_precedence(lex_token_t *token, lex_data_t *data, stable_data
 					stack_pop(&stack);
 					stack_push(&stack, '<');
 					stack_push(&stack, 'E');
-					if((token->type == LEX_INTEGER) || (token->type == LEX_DOUBLE))
-					{
-					   stack_push(&stack_types, token->type);
-					   constant_check(token);
-					}
-					else if(token->type == LEX_IDENTIFIER)
-					{
-						int *pom;
-						pom = &token->val;
-						stable.active.
-						offset = offset*(-1);
-						instr_insert_instr(&list,INST_, pom, 0, offset )
-					}
 					stack_push(&stack, get_sign(token));
 
 				}
 				else
 				{
 					stack_push(&stack,'<');
-					if((token->type == LEX_INTEGER) || (token->type == LEX_DOUBLE))
+					if((token->type == LEX_INTEGER) || (token->type == LEX_DOUBLE) || (token->type == LEX_STRING))
 					{
 						stack_push(&stack_types, token->type);
-						constant_check(token);
+						constant_check(token, &stack_index);
 					}
-					//else if(token->type == LEX_IDENTIFIER)
-						//instr_insert_instr(, INST_ASSIGN, , , )
+					else if(token->type == LEX_IDENTIFIER)
+					{
+						stable_search_scopes(&symbol_table, token->val, &ptr_data);
+						int offset = symbol_table.active->stack_idx++;
+						instr_insert_instr(&list, INSTR_MOVD, offset, ptr_data->var.offset , 0);
+						stack_push(&stack_index, offset);
+					}
 					stack_push(&stack, get_sign(token));
 				}
 
@@ -347,46 +351,47 @@ lex_token_t *syntax_precedence(lex_token_t *token, lex_data_t *data, stable_data
 					switch(stack_top(&stack))
 					{
 						case sign_not_equal:
-							type(&stack, &stack_types,E_notequal_E);
+							type(&stack, &stack_types, &stack_index, INSTR_NEQ);
 							break;
 
 						case sign_equal:
-							type(&stack, &stack_types, E_equal_E);
+							type(&stack, &stack_types, &stack_index, INSTR_EQ);
 							break;
 
 						case sign_greater_equal:
-							type(&stack, &stack_types, E_greaterequal_E);
+							type(&stack, &stack_types, &stack_index, INSTR_GTE);
 							break;
 
 						case sign_less_equal:
-							type(&stack, &stack_types, E_lessequal_E);
+							type(&stack, &stack_types, &stack_index, INSTR_LTE);
 							break;
 
 						case sign_less:
-							type(&stack, &stack_types, E_less_E);
+							type(&stack, &stack_types, &stack_index, INSTR_LT);
 							break;
 
 						case sign_greater:
-							type(&stack, &stack_types, E_greater_E);
+							type(&stack, &stack_types, &stack_index, INSTR_GT);
 							break;
 
 						case sign_slash:
-							type(&stack, &stack_types, E_div_E);
+							type(&stack, &stack_types, &stack_index, INSTR_DIV);
 							break;
 
 						case sign_times:
-							type(&stack, &stack_types, E_mul_E);
+							type(&stack, &stack_types, &stack_index, INSTR_MUL);
 							break;
 
 						case sign_minus:
-							type(&stack, &stack_types, E_minus_E);
+							type(&stack, &stack_types, &stack_index, INSTR_SUB);
 							break;
 						case sign_plus:
-							type(&stack, &stack_types, E_plus_E);
+							type(&stack, &stack_types, &stack_index, INSTR_ADD);
 						default:
 							fprintf(stderr, "Error all");
 					}
 				}
+				
 			case '#':
 
 				/* Check for function call, if ID( then we will expect it to be function and return token to syntax. analyator
@@ -394,12 +399,13 @@ lex_token_t *syntax_precedence(lex_token_t *token, lex_data_t *data, stable_data
 				if(stack_top(&stack) == symbol_id)
 				{
 					if(get_sign(token) == sign_lparen)
-						return token;
+						return -1;
 						
 					else
 						fprintf(stderr, "Error, variable after variable is not allowed without sign between them");
 				}
 		}
+
 		if(token->type == LEX_IDENTIFIER)
 		{
 			if(syntax_data->id != NULL)
@@ -411,7 +417,7 @@ lex_token_t *syntax_precedence(lex_token_t *token, lex_data_t *data, stable_data
 	}while(!(((stack_top(&stack) == 'E') && (stack_offset(&stack, 2) == symbol_dollar)) && ((get_sign(token) == sign_rparen) || (token->type ==
 						LEX_SEMICOLON))));
 
-return token;	
+return final_index;	
 }
 
 
--- src/expr.h
+++ src/expr.h
@@ -10,9 +10,6 @@
 #include "error.h"
 #include "Estack.h"
 
-
-
-
 typedef enum 
 {
 	sign_plus,
@@ -32,8 +29,6 @@ typedef enum
 	symbol_semicolon,
 	symbol_eof
 }sign_value;
-
-
 typedef enum 
 {
 	E_plus_E, 			// E->E+E  0
@@ -55,10 +50,8 @@ enum symbols
 	nonterminal,
 	symbol_error
 };
-lex_token_t *syntax_precedence(lex_token_t *token, lex_data_t *data, stable_data_t *symbol_data, stable_data_t
-		*ptr_data, syntax_data_t *syntax_data);
+int syntax_precedence(lex_token_t *token, lex_data_t *data, stable_data_t *symbol_data, stable_data_t*ptr_data, syntax_data_t *syntax_data);
 sign_value get_sign(lex_token_t *t);
-
 
 
 #endif
